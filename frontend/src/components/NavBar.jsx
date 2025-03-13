5dac1e028f0f41fe6d48abde2e779fca30ab8f9c
GlebNes109 <testnesyutin@gmail.com>
2025-03-13 22:06:00 +0300

    наверстал упущенное

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <nav className={styles.navbar}>
             <h1>FindMyTeam</h1>
-            <button className={styles.navbarBtn}>Поддержка</button>
+            <button className={styles['navbar-btn-support']}>Поддержка</button>
         </nav>
     );
 }
