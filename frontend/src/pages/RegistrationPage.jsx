d431b1e48577b01e86ba42ea5058dfda73688833
GlebNes109 <testnesyutin@gmail.com>
2025-03-14 11:23:27 +0300

    переделаны логин и регистрация, теперь есть выпадайка с ролью юзера, но теперь размер полей в логине и регистрации разный, но это mvp, поэтому пока что пофиг

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "../styles/LoginRegisterPage.module.css"; // Подключаем стили
 
+
 const RegistrationPage = () => {
+    const [selectedOption, setSelectedOption] = useState(""); // Состояние для хранения выбранного варианта
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value); // Обновляем состояние при выборе
+    };
+
     return (
         <div className={styles['main-content']}>
             <div className={styles['fields-content']}>
@@ -10,6 +17,13 @@ const RegistrationPage = () => {
                 <input type="text" placeholder="Введите логин" className={styles["input-field"]}/>
                 <input type="password" placeholder="Введите пароль" className={styles["input-field"]}/>
                 <input type="password" placeholder="Повторите пароль" className={styles["input-field"]}/>
+                <div>
+                    <select className={styles["select"]} id="options" value={selectedOption} onChange={handleChange}>
+                        <option value="">Выберите свою роль:</option>
+                        <option value="option1">Капитан команды</option>
+                        <option value="option2">Участник</option>
+                    </select>
+                </div>
                 <button type="submit" className={styles["login-button"]}>Войти</button>
             </div>
         </div>
