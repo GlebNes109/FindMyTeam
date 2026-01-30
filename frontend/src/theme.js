b8d064c1b7d25cfa3e2e1f47ff29a7e8d909a6a5
GlebNes109 <testnesyutin@gmail.com>
2026-01-30 13:29:11 +0300

    feat (frontend): ui small improvements

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,26 +1,18 @@
-import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
-import { cyan, teal } from '@mui/material/colors';
+import { extendTheme as extendTheme } from '@mui/material/styles';
 
 const theme = extendTheme({
     colorSchemes: {
-        dark: {
-            palette: {
-                mode: 'dark',
-                primary: { main: '#FFDD2D' },
-                secondary: { main: '#FFFFFF' },
-                background: { default: '#121212', paper: '#1C1B1F' },
-                text: { primary: '#FFFFFF', secondary: '#B3B3B3' },
-            },
-        },
-        /*light: {
+        /* light: {
             palette: {
                 mode: 'light',
-                primary: {main: '#FFDD2D'},
-                secondary: {main: '#f3f3f3'},    // чёрный
-                background: {default: '#FFFFFF', paper: '#FFFBFE'}, // белый фон
-                text: {primary: '#000000', secondary: '#333333'},
+                primary: { main: '#FFDD2D' },
+                secondary: { main: '#1f1f1f' },
+                background: { default: '#f7f7f7', paper: '#ffffff' },
+                text: { primary: '#1a1a1a', secondary: '#4a4a4a' },
             },
         },*/
+        dark: { palette: { mode: 'dark', primary: { main: '#FFDD2D' }, secondary: { main: '#FFFFFF' }, background: { default: '#121212', paper: '#1C1B1F' }, text: { primary: '#FFFFFF', secondary: '#B3B3B3' }, }, },
+
     },
     typography: {
         fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
@@ -31,6 +23,17 @@ const theme = extendTheme({
     },
     shape: {
         borderRadius: 12,
+    },
+    components: {
+        MuiAppBar: {
+            defaultProps: { color: 'default' },
+            styleOverrides: {
+                root: ({ theme }) => ({
+                    backgroundColor: theme.palette.background.paper,
+                    color: theme.palette.text.primary,
+                }),
+            },
+        },
     },
 });
 
