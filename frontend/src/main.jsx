f3009cf51994dbf7fac9fe1acd6ede2d741e41c8
Gleb Nesyutin <129966254+GlebNes109@users.noreply.github.com>
2026-01-31 01:04:02 +0300

    feat: inactive events on frontend now (#33)
    * feat(frontend): inactive events (#32)
    
    * feat (frontend): ui small improvements
    
    * fix(frontend): small bugfix and refactoring on frontend
    
    * feat(frontend): menu with inactive events on EventsPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import {CssBaseline} from '@mui/material';
+import { CssVarsProvider } from '@mui/material/styles';
 import theme from './theme';
 import {ToastProvider} from "./components/ToastProvider.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <ThemeProvider theme={theme}>
+      <CssVarsProvider theme={theme} defaultMode="system" modeStorageKey="themeMode">
           <CssBaseline />
-          <ToastProvider>
-          <App />
-          </ToastProvider>
-      </ThemeProvider>
+              <App />
+      </CssVarsProvider>
   </StrictMode>,
 )
